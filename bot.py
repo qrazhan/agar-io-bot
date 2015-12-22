de90b31cf75b963e9e25097a86ca8d8cf78c79c1
Prashan Dharmasena <prashan14@gmail.com>
2015-12-21 19:15:23 -0500

    moving to ws4py

--- bot.py
+++ bot.py
@@ -1,23 +1,41 @@
-import asyncio
-import websockets
+from ws4py.client.threadedclient import WebSocketClient
 import requests
 
 def getIpPortAndToken():
 	r = requests.post('http://m.agar.io', data='US-Atlanta\n154669603')
+	print(r.text)
 	rs = r.text.split('\n')
 	ip = rs[0].split(':')[0]
 	port = rs[0].split(':')[1]
 	token = rs[1]
 	return ip, port, token
 
-@asyncio.coroutine
-def hello():
-	ip, port, token = getIpPortAndToken()
-	print('Attempting to connect to: '+'ws://'+ip+':'+port+'/')
-	websocket = yield from websockets.connect('ws://'+ip+':'+port+'/', extra_headers={'Origin':'http://agar.io'})
-	yield from websocket.send('80'+token)
-	while True:
-		r = yield from websocket.recv()
-		print(r)
+class AgarClient(WebSocketClient):
+    def opened(self):
+       self.send('80'+self.token)
 
-asyncio.get_event_loop().run_until_complete(hello())
+    def closed(self, code, reason=None):
+        print("Closed down", code, reason)
+
+    def received_message(self, m):
+        print(m)
+    
+    def setToken(self, token):
+    	self.token = token
+
+if __name__ == '__main__':
+    try:
+    	ip, port, token = getIpPortAndToken()
+    	url = 'ws://'+ip+':'+port+'/'
+    	print(url, token)
+    	ws = AgarClient(url, headers=[('Origin', 'http://agar.io')])
+    	if not any(x for x in [('Origin', 'http://agar.io')] if x[0].lower() == 'origin'):
+    		print('adding unnecessary header')
+    	print('what ws client is actually sending:')
+    	
+    	ws.key = token.encode()
+    	print(ws.host, ws.port, ws.key.decode())
+    	ws.connect()
+    	ws.run_forever()
+    except KeyboardInterrupt:
+    	ws.close()
